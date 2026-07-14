8f816a16d8b5c00fa3e66856921b6d172591c5b4
Sajda Maryam <sajdamaryam6@gmail.com>
2026-07-14 21:30:40 +0500

    Main structure of Code

--- SourceCode.cpp
+++ SourceCode.cpp
@@ -1,8 +1,71 @@
-//Start Code!!
+// Start Code!!
 #include <iostream>
+#include<iomanip>
+#include<fstream>
+#include<string>
 using namespace std;
-int main (){
-    cout<<"=== Welcome To MoneyMap ==="<<endl;
 
+struct Expense{
+	int id;
+	string type, category, title, priority;
+	float amount;
+	string date;
+};
+
+int MAX_RECORDS = 50;
+Expense* records = NULL;
+int record_count = 0;
+string fileName = "MyMoneyMap.txt";
+
+bool login();
+void loadFromFile();
+void saveToFile();
+void addRecord();
+void viewRecords();
+void updateRecord();
+void deleteRecord();
+void summary();
+void menu();
+bool returnToMenu();
+int main()
+{
+    records = new Expense[MAX_RECORDS]; // dynamic memory allocation
+    login();
+    loadFromFile();
+    int choice;
+    bool backToMenu = true;
+    while (backToMenu)
+    {
+        menu();
+        cout << "\n=== Enter choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            addRecord();
+            break;
+        case 2:
+            viewRecords();
+            break;
+        case 3:
+            updateRecord();
+            break;
+        case 4:
+            deleteRecord();
+            break;
+        case 5:
+            summary();
+            break;
+        case 0:
+            saveToFile();
+            cout << "\n----- Program Exited -----" << endl;
+            return 0;
+        default:
+            cout << "-- Invalid choice! --" << endl;
+        }
+        backToMenu = returnToMenu();
+    }
+    cout << "\n----- Program Exited -----" << endl;
+    delete[] records; // free dynamic memory
     return 0;
 }
